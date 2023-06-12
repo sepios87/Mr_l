fb0fb9ed98a0d8605279fa09ce937e282ed5733f
florian <florian.toribio@gmail.com>
2023-06-12 14:16:21 +0200

    add icons

--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -1,10 +1,26 @@
-<section class="homeHeader">
+<header class="homeHeader">
     <img class="homeHeader__logo" src="assets/logo.png">
     <h1 class="homeHeader__title">le hot dog à la saucisse de toulouse</h1>
     <a class="button homeHeader__button" href="#">Découvre nos recettes</a>
-</section>
+</header>
 
-<section class="homeFresh">
-    <h2>Des produits frais et locaux</h2>
-    <h3>On prend à coeur de travailler avec un maximum de produits frais et locaux</h3>
+<section class="homeSupplier">
+    <h2 class="homeSupplier__title">Des produits frais et locaux</h2>
+    <h3 class="homeSupplier__explain">On prend à coeur de travailler avec un maximum de produits frais et locaux</h3>
+    <ul>
+        <li>
+            <figure class="homeSupplierItem">
+                <p class="homeSupplierItem__title">La boulange d'Angéline</p>
+                <img src="assets/suppliers/bread.jpg">
+                <figcaption>
+                    Pain ultra moelleux, méga bon.
+                    <br/>
+                    Coeur sur Guillaume et Antoine.
+                    <br/>
+                    37 av. de l'URSS, 31400 Toulouse
+                </figcaption>
+            </figure>
+        </li>
+
+</section>
 </section>
