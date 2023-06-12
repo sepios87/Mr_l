49e0f1e6a453f382cc39e23492dc9657d90fa4b3
mdeleest <manon.deleest@etudiant.univ-lr.fr>
2023-06-12 15:03:37 +0200

    WIP contact

--- src/pages/contact/contact.php
+++ src/pages/contact/contact.php
@@ -4,5 +4,38 @@
 
     <span class="contact__sub-title">Hot-dog & love</span>
 
-    <h2 class="contact__adress"><a href="https://goo.gl/maps/EkaBsxkriKoesbj48" target="_blank">8 rue du languedoc &nbsp; 31000&nbsp;toulouse</a></h2>
+    <h2 class="contact__adress"><a href="https://goo.gl/maps/EkaBsxkriKoesbj48" target="_blank">8 rue du languedoc 31000 toulouse</a></h2>
 </header>
+<main class="contactMain">
+    <div>
+        <ul class="contactMain__schedule">
+            <li class="contactMain__item">
+                <span class="contactMain__title">Lundi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+                <span class="contactMain__title">Mardi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+                <span class="contactMain__title">Mercredi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+                <span class="contactMain__title">Jeudi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+            <span class="contactMain__title">Vendredi</span> <p> 11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+            <span class="contactMain__title">Samedi</span> <p> 11H30 - 14H / 19H - 21H30</p>
+            </li>
+            <li class="contactMain__item">
+                <span class="contactMain__title">Dimanche</span> <p> FERME </p>
+            </li>
+        </ul>
+        <h4 class="contactMain__info">On est aussi aux <span class="contactMain__info--accent">HALLES DE LA CITÉ</span> Du mardi au Samedi soir ! </h4>
+    </div>
+    <ul class="contactMain-network">
+        <li class="contactMain-network__item" > <a href="https://www.instagram.com/mrl_hotdog/" target="_blank">Instagram</a> </li>
+        <li class="contactMain-network__item" > <a href="mailto:jolimonsieur.l@gmail.com">mail</a> </li>
+        <li class="contactMain-network__item" > <a href="tel:+33647378377">06 47 37 83 77</a> </li>
+    </ul>
+</main>
