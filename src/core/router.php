cfda8dfe3fe4986cb87040474aed9352b939a614
florian <florian.toribio@gmail.com>
2023-06-13 15:34:46 +0200

    meta

--- src/core/router.php
+++ src/core/router.php
@@ -3,20 +3,41 @@ $origin = str_replace(dirname($_SERVER['PHP_SELF']), '', $_SERVER['REQUEST_URI']
 
 switch ($origin) {
     case '/':
-        displayPage('home');
+        displayPage(
+            'home',
+            'Hotdog toulouse  - Monsieur L',
+            'Découvrez un délicieux paradis des hot-dogs ! Notre restaurant de restauration rapide Mr.L vous propose des hot-dogs préparés avec des ingrédients frais et de qualité.'
+        );
         break;
     case '/contact':
-        displayPage('contact');
+        displayPage(
+            'contact',
+            'Contactez-nous - Monsieur L',
+            'Contactez Mr.L, le roi du hot-dog toulousain ! Envoyez-nous un e-mail, suivez-nous sur notre page Instagram ou appelez-nous directement.'
+        );
         break;
     case '/event':
-        displayPage('event');
+        displayPage(
+            'event',
+            'Hot dog toulouse, évènement - Monsieur L',
+            "Envie d'ajouter une touche savoureuse à votre prochain évènement ? Contactez Monsieur L, expert en hot dogs, pour une expérience inoubliable !",
+        );
         break;
     default:
-        displayPage('404');
+        displayPage(
+            '404',
+            'Erreur 404 - Oups ! Vous avez déviez de site Monsieur L',
+            "Désolé, nous avons égaré votre hot-dog ! Vous êtes tombé sur notre page d'erreur 404. Revenez à notre menu savoureux de hot-dogs artisanaux."
+        );
         break;
 }
 
-function displayPage($pageName)
+// Display the page
+// Arguments are used in the header page
+// $pageName: the name of the page to display
+// $title: the title of the page
+// $description: the description of the page
+function displayPage($pageName, $title = 'Mr.l - Le roi du Hot Dog', $description = '')
 {
     require_once 'src/shared/header.php';
     require_once 'src/pages/' . $pageName . '/' . $pageName . '.php';
--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -1,11 +1,11 @@
 <header class="home-header">
-    <img class="home-header__logo" src="assets/img/logo.png" alt="Logo du restaurant" />
+    <img class="home-header__logo" src="assets/img/logo.png" alt="Logo du restaurant">
     <h1 class="home-header__title">le hot dog à la saucisse de toulouse</h1>
     <a class="button home-header__button" href="#restaurant-menu">Découvre nos recettes</a>
 </header>
 
 <section class="home-supplier">
-    <h2 class="home-supplier__title">Des produits <br /> frais et locaux</h2>
+    <h2 class="home-supplier__title">Des produits <br> frais et locaux</h2>
     <ul class="home-supplier__list">
         <li class="home-supplier-item--explain">
             <h3 class="home-supplier__explain">On prend à coeur de travailler avec un maximum de produits frais et locaux</h3>
@@ -13,10 +13,10 @@
         <li class="home-supplier-item">
             <figure>
                 <p class="home-supplier-item__title">La boulange d'Angéline</p>
-                <img class="home-supplier-item__img" src="assets/img/suppliers/pain_boulangerie_angeline.jpg" alt="Pain de la boulangerie d'Angéline" />
+                <img class="home-supplier-item__img" src="assets/img/suppliers/pain_boulangerie_angeline.jpg" alt="Pain de la boulangerie d'Angéline">
                 <figcaption class="home-supplier-item__figcaption">
-                    Pain ultra moelleux, méga bon. <br />
-                    Coeur sur Guillaume et Antoine. <br />
+                    <strong>Pain ultra moelleux</strong>, méga bon. <br>
+                    Coeur sur Guillaume et Antoine. <br>
                     37 av. de l'URSS, 31400 Toulouse
                 </figcaption>
             </figure>
@@ -24,16 +24,16 @@
         <li class="home-supplier-item">
             <figure>
                 <p class="home-supplier-item__title">Agriculture locale</p>
-                <img class="home-supplier-item__img" src="assets/img/suppliers/herbe_aromatique_locale.jpg" alt="Herbes aromatiques locales" />
+                <img class="home-supplier-item__img" src="assets/img/suppliers/herbe_aromatique_locale.jpg" alt="Herbes aromatiques locales">
                 <figcaption class="home-supplier-item__figcaption">
-                    Nos herbes fraiches et légumes proviennent de l'agriculture locale
+                    Nos herbes fraiches et légumes proviennent de <strong>l'agriculture locale</strong>
                 </figcaption>
             </figure>
         </li>
         <li class="home-supplier-item">
             <figure>
                 <p class="home-supplier-item__title">Ketchup Sanchon</p>
-                <img class="home-supplier-item__img" src="assets/img/suppliers/ketchup_sanchon.jpg" alt="Ketchup Sanchon" />
+                <img class="home-supplier-item__img" src="assets/img/suppliers/ketchup_sanchon.jpg" alt="Ketchup Sanchon">
                 <figcaption class="home-supplier-item__figcaption">
                     Des sauces locales et savoureuses tout droit venues de chez <a class="home-supplier-item__link" href="https://www.marcelsenchou.fr/" target="_blank">les ariègeois</a>.
                 </figcaption>
@@ -42,10 +42,10 @@
         <li>
             <figure class="home-supplier-item">
                 <p class="home-supplier-item__title">Boucherie Côtelette</p>
-                <img class="home-supplier-item__img" src="assets/img/suppliers/stephane_boucherie_cotelette.jpg" alt="Stéphane de la boucherie Côtelette" />
+                <img class="home-supplier-item__img" src="assets/img/suppliers/stephane_boucherie_cotelette.jpg" alt="Stéphane de la boucherie Côtelette">
                 <figcaption class="home-supplier-item__figcaption">
-                    C'est chez Stéphane à la <a class="home-supplier-item__link" href="https://www.instagram.com/boucherie.cotelette/" target="_blank">boucherie cotelette</a> qu'on chope notre super saucisse de Toulouse. BIG diamêtre pour un BIG kiff. <br />
-                    22 av. de l'URSS, 31400 Toulouse <br />
+                    C'est chez Stéphane à la <a class="home-supplier-item__link" href="https://www.instagram.com/boucherie.cotelette/" target="_blank">boucherie cotelette</a> qu'on chope notre super saucisse de Toulouse. BIG diamêtre pour un BIG kiff. <br>
+                    22 av. de l'URSS, 31400 Toulouse <br>
                 </figcaption>
             </figure>
         </li>
@@ -53,8 +53,8 @@
 </section>
 
 <section class="home-restaurant-menu" id="restaurant-menu">
-    <h2 class="home-restaurant-menu__title">Une carte avec <br /> des recettes cools</h2>
-    <h3 class="home-restaurant-menu__explain">ZiG ZaG de moutarde américaiNe sur Nos Hot-doGs à la saucisse de toulouse</h3>
+    <h2 class="home-restaurant-menu__title">Une carte avec <br> des recettes cools</h2>
+    <h3 class="home-restaurant-menu__explain">Zig zag de moutarde américaine sur nos Hot-dogs à la saucisse de toulouse</h3>
     <div class="home-restaurant-menu__container">
         <div>
             <ul class="home-restaurant-menu__list">
@@ -127,9 +127,9 @@
                 </li>
             </ul>
             <p class="home-restaurant-menu__note">
-                <span>*Petites notes :</span><br />
-                - Nos Pains sont artisanaux et locaux<br />
-                - Notre saucisse de toulouse vient de chez cotelette (2km)<br />
+                <span>*Petites notes :</span><br>
+                - Nos Pains sont artisanaux et locaux<br>
+                - Notre saucisse de toulouse vient de chez cotelette (2km)<br>
                 - Nos ketchups viennent d'ariège
             </p>
         </div>
@@ -138,41 +138,41 @@
 
 </section>
 
-<section class="home-gallery">
+<div class="home-gallery">
     <ul class="home-gallery__list">
         <li class="home-gallery__item">
-            <img class="home-gallery__img" src="assets/img/gallery/ingredients_hot_dog.jpg" alt="Ingrédients pour faire un hot dog" />
+            <img class="home-gallery__img" src="assets/img/gallery/ingredients_hot_dog.jpg" alt="Ingrédients pour faire un hot dog">
         </li>
         <li class="home-gallery__item">
-            <img class="home-gallery__img" src="assets/img/gallery/hot_dog_toutouyoutou.jpg" alt="Hot dog toutouyoutou" />
+            <img class="home-gallery__img" src="assets/img/gallery/hot_dog_toutouyoutou.jpg" alt="Hot dog toutouyoutou">
         </li>
         <li class="home-gallery__item">
-            <img class="home-gallery__img" src="assets/img/gallery/hot_dog_woaf_the_fuck.jpg" alt="Hot dog woaf the fuck"/>
+            <img class="home-gallery__img" src="assets/img/gallery/hot_dog_woaf_the_fuck.jpg" alt="Hot dog woaf the fuck">
         </li>
         <li class="home-gallery__item home-gallery__item--desktop">
-            <img class="home-gallery__img" src="assets/img/gallery/baby_hot_dogs.jpg" alt="Lots de trois petits hot-dogs"/>
+            <img class="home-gallery__img" src="assets/img/gallery/baby_hot_dogs.jpg" alt="Lots de trois petits hot-dogs">
         </li>
         <li class="home-gallery__item home-gallery__item--desktop">
-            <img class="home-gallery__img" src="assets/img/gallery/pop_des_sables.jpg" alt="Bâtonnet de pop-corns enrobés de chocolat"/>
+            <img class="home-gallery__img" src="assets/img/gallery/pop_des_sables.jpg" alt="Bâtonnet de pop-corns enrobés de chocolat">
         </li>
         <li class="home-gallery__item home-gallery__item--desktop">
-            <img class="home-gallery__img" src="assets/img/gallery/nachos-monsieur-l.jpg" alt="Barquette de nachos avec sa sauce"/>
+            <img class="home-gallery__img" src="assets/img/gallery/nachos-monsieur-l.jpg" alt="Barquette de nachos avec sa sauce">
         </li>
         <li class="home-gallery__item home-gallery__item--desktop">
-            <img class="home-gallery__img" src="assets/img/gallery/pain_moelleux_boulangerie_angeline.jpg" alt="Pain tressé marbré"/>
+            <img class="home-gallery__img" src="assets/img/gallery/pain_moelleux_boulangerie_angeline.jpg" alt="Pain tressé marbré">
         </li>
     </ul>
-</section>
+</div>
 
 <section class="home-lifestyle">
-    <img class="home-lifestyle__mrl" src="assets/img/mrl.png" alt="Titre monsieurl"/>
+    <img class="home-lifestyle__mrl" src="assets/img/mrl.png" alt="Titre monsieurl">
     <span class="home-lifestyle__slogan">Lifestyle</span>
     <h2 class="home-lifestyle__title">MR.L c'est + que de la street food</h2>
     <div class="home-lifestyle__explain">
-        <h3>Viens shopper au resto nos <br /> tee-shirts et assiettes</h3>
+        <h3>Viens shopper au resto nos <br> tee-shirts et assiettes</h3>
         <span>
             Au <a href="https://goo.gl/maps/EkaBsxkriKoesbj48" target="_blank">8 rue du Languedoc</a>
-            <br /> Ou par DM sur <a href="https://www.instagram.com/mrl_hotdog/" target="_blank">notre insta</a> pour commander
+            <br> Ou par DM sur <a href="https://www.instagram.com/mrl_hotdog/" target="_blank">notre insta</a> pour commander
         </span>
     </div>
 </section>
@@ -182,30 +182,30 @@
     <ul class="home-tee-shirt__list">
         <li class="home-tee-shirt__item">
             <figure class="home-tee-shirt__figure">
-                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_hot_dog.jpg" alt="Tee-shirt hot-dog" />
+                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_hot_dog.jpg" alt="Tee-shirt hot-dog">
                 <figcaption class="home-tee-shirt__caption">
-                    <strong>Modèle HOT-DOG</strong><br />
-                    Du XS au L<br />
+                    <strong>Modèle HOT-DOG</strong><br>
+                    Du XS au L<br>
                     29 euros
                 </figcaption>
             </figure>
         </li>
         <li class="home-tee-shirt__item">
             <figure class="home-tee-shirt__figure">
-                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_stc.jpg" alt="Tee-shirt STC" />
+                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_stc.jpg" alt="Tee-shirt STC">
                 <figcaption class="home-tee-shirt__caption">
-                    <strong>Modèle STC</strong><br />
-                    Du XS au L <br />
+                    <strong>Modèle STC</strong><br>
+                    Du XS au L <br>
                     29 euros
                 </figcaption>
             </figure>
         </li>
         <li class="home-tee-shirt__item">
             <figure class="home-tee-shirt__figure">
-                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_trop_bon.jpg" alt="Tee-shirt trop bon" />
+                <img class="home-tee-shirt__img" src="assets/img/tee-shirt/tee-shirt_trop_bon.jpg" alt="Tee-shirt trop bon">
                 <figcaption class="home-tee-shirt__caption">
-                    <strong>Modèle Trop Bon</strong><br />
-                    Du XS au L <br />
+                    <strong>Modèle Trop Bon</strong><br>
+                    Du XS au L <br>
                     29 euros
                 </figcaption>
             </figure>
@@ -216,17 +216,17 @@
 <section class="home-plate">
     <h2 class="home-plate__title">Nos assiettes</h2>
     <p class="home-plate__explain">
-        Assiettes faites à la main<br />
-        75 euros la douzaine<br />
-        7.5 euros l'unité<br />
+        <strong>Assiettes faites à la main</strong><br>
+        75 euros la douzaine<br>
+        7.5 euros l'unité<br>
         *Quantité très limitée
     </p>
     <ul class="home-plate__list">
         <li class="home-plate__item">
-            <img class="home-plate__img" src="assets/img/plate/boite_assiettes.jpg" alt="Boite d'assiettes"/>
+            <img class="home-plate__img" src="assets/img/plate/boite_assiettes.jpg" alt="Boite d'assiettes">
         </li>
         <li class="home-plate__item">
-            <img class="home-plate__img" src="assets/img/plate/exposition_assiettes.jpg" alt="Exposition d'assiettes"/>
+            <img class="home-plate__img" src="assets/img/plate/exposition_assiettes.jpg" alt="Exposition d'assiettes">
         </li>
     </ul>
 </section>
