c0ce3a7967f3a3b4beb5bce8c1cf56acbe12ace1
mdeleest <manon.deleest@etudiant.univ-lr.fr>
2023-06-13 09:54:23 +0200

    404

--- src/pages/404/404.php
+++ src/pages/404/404.php
@@ -1,8 +1,6 @@
 <section class="not-found">
-    <!-- <img src="assets/img/mrl.png" alt="Mr L" class="not-found__img-header"> -->
     <h1 class="not-found__title">404</h1>
     <p class="not-found__sub-title">Tu sors ou j'te sors ? </p>
-    <!-- <img src="assets/img/404.png" alt="Image de 404" class="not-found__image">    -->
     <p class="not-found__texte">Tu as du te tromper de chemin, notre resto n'est pas par là, retourne à l'accueil et on fait comme si de rien n'était ! </p>
 
     <a class="button" href="#">Retourner à la page d'acceuil</a>
