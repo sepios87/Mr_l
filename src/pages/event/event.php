64f33333827ebad13632fb7541435eef61b04691
mdeleest <manon.deleest@etudiant.univ-lr.fr>
2023-06-13 10:24:31 +0200

    WIP event

--- src/pages/event/event.php
+++ src/pages/event/event.php
@@ -1,1 +1,9 @@
-<p>Event Page</p>
+<header class="event-header">
+    <img class="event-header__logo" src="assets/img/logo.png" alt="Logo du restaurant" />
+    <div class="event-header__title">
+        <h1>MR.L est une marque de street good food. </h1>
+        <h2>Des produits sexys, locaux et gourmands.</h2>
+    </div>
+    <p class="event-header__title">Fondé en 2020, MR.L est le roi du hot-dog Toulousain.</p>
+    <a class="button event-header__button" href="#restaurant-menu">Découvre nos recettes</a>
+</header>
