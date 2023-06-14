7a99f907480268bc28b129406f24f62acd107976
florian <florian.toribio@gmail.com>
2023-06-14 12:04:09 +0200

    contact

--- src/pages/contact/contact.php
+++ src/pages/contact/contact.php
@@ -1,48 +1,60 @@
-
 <header class="contact">
     <h1 class="contact__title">contact</h1>
-
-    <span class="contact__sub-title">Hot-dog & love</span>
-
-    <h2 class="contact__adress"><a href="https://goo.gl/maps/EkaBsxkriKoesbj48" target="_blank">8 rue du languedoc 31000 toulouse</a></h2>
+    <span class="contact__subtitle">Hot-dog & love</span>
+    <ul class="contact-socials">
+        <li class="contact-socials__item"> <a href="https://www.instagram.com/mrl_hotdog/" target="_blank">Instagram @mrl_hotdog</a> </li>
+        <li class="contact-socials__item"> <a href="mailto:jolimonsieur.l@gmail.com" rel="noopener">jolimonsieur.l@gmail.com</a> </li>
+        <li class="contact-socials__item"> <a href="tel:+33647378377" rel="noopener">06 47 37 83 77</a> </li>
+    </ul>
 </header>
 <main class="contact-main">
-    <div>
-        <ul class="contact-main__schedule">
-            <li class="contact-main__item">
-                <span class="contact-main__title">Lundi</span> <p>11H30 - 14H / 19H - 21H30</p>
+    <div class="contact-address">
+        <h2 class="contact-address__title"><a href="https://goo.gl/maps/EkaBsxkriKoesbj48" target="_blank">8 rue du languedoc 31000 toulouse</a></h2>
+        <iframe class="contact-address__iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11558.210831049193!2d1.4452624!3d43.5950323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebd9108d90df3%3A0x4f1f862546ce80ab!2sMR.L!5e0!3m2!1sfr!2sfr!4v1686732720794!5m2!1sfr!2sfr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="contact-schedule">
+        <h2 class="contact-schedule__title">Horaires</h2>
+        <ul class="contact-schedule__list">
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Lundi</span>
+                <p>11H30 - 14H / 19H - 21H30</p>
             </li>
-            <li class="contact-main__item">
-                <span class="contact-main__title">Mardi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Mardi</span>
+                <p>11H30 - 14H / 19H - 21H30</p>
             </li>
-            <li class="contact-main__item">
-                <span class="contact-main__title">Mercredi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Mercredi</span>
+                <p>11H30 - 14H / 19H - 21H30</p>
             </li>
-            <li class="contact-main__item">
-                <span class="contact-main__title">Jeudi</span> <p>11H30 - 14H / 19H - 21H30</p>
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Jeudi</span>
+                <p>11H30 - 14H / 19H - 21H30</p>
             </li>
-            <li class="contact-main__item">
-            <span class="contact-main__title">Vendredi</span> <p> 11H30 - 14H / 19H - 21H30</p>
+            <l class="contact-schedule__item">
+                <span class="contact-schedule__day">Vendredi</span>
+                <p> 11H30 - 14H / 19H - 21H30</p>
+            </l>
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Samedi</span>
+                <p> 11H30 - 14H / 19H - 21H30</p>
             </li>
-            <li class="contact-main__item">
-            <span class="contact-main__title">Samedi</span> <p> 11H30 - 14H / 19H - 21H30</p>
-            </li>
-            <li class="contact-main__item">
-                <span class="contact-main__title">Dimanche</span> <p> FERME </p>
+            <li class="contact-schedule__item">
+                <span class="contact-schedule__day">Dimanche</span>
+                <p> FERME </p>
             </li>
         </ul>
-        <h4 class="contact-main__info">On est aussi aux <span class="contact-main__info--accent">HALLES DE LA CITÉ</span> Du mardi au Samedi soir ! </h4>
+        <h4 class="contact-schedule__info">On est aussi aux <a href="https://goo.gl/maps/SsMg4vSSEApq5uGS6" target="_blank">HALLES DE LA CITÉ</a> Du mardi au Samedi soir ! </h4>
     </div>
-    <ul class="contact-main-network">
-        <li class="contact-main-network__item" > <a href="https://www.instagram.com/mrl_hotdog/" target="_blank">Instagram</a> </li>
-        <li class="contact-main-network__item" > <a href="mailto:jolimonsieur.l@gmail.com" rel="noopener">Mail</a> </li>
-        <li class="contact-main-network__item" > <a href="tel:+33647378377" rel="noopener">06 47 37 83 77</a> </li>
-        <li class="contact-main-network__contain-logo"> 
+    <ul class="contact-main-delivery">
+        <li class="contact-main-delivery__item">
             <a href="https://www.ubereats.com/fr/store/mr-l-hot-dog/VC3b85g4QCSp2dLENSGZmA?diningMode=DELIVERY" target="_blank" title="Redirection vers Uber Eat">
-                <img class="contact-main-network__logo" src="assets/img/icons/socials/uber_eats.png" alt="Logo de Uber Eat">
+                <img class="contact-main-delivery__logo" src="assets/img/icons/socials/uber_eats.png" alt="Logo de Uber Eat">
             </a>
+        </li>
+        <li class="contact-main-delivery__item">
             <a href="https://deliveroo.fr/fr/menu/toulouse/carmes-saint-etienne/mr-l-hot-dog?day=today&geohash=spc00bssurnd&time=ASAP" target="_blank" title="Redirection vers Deliveroo">
-                <img class="contact-main-network__logo" src="assets/img/icons/socials/deliveroo.png" alt="Logo de Deliveroo">
+                <img class="contact-main-delivery__logo" src="assets/img/icons/socials/deliveroo.png" alt="Logo de Deliveroo">
             </a>
         </li>
     </ul>
