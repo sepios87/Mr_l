c7b285931f45695e9211916f13251c1d61ab3441
mdeleest <manon.deleest@etudiant.univ-lr.fr>
2023-06-15 13:28:53 +0200

    design login

--- src/pages/login/login.php
+++ src/pages/login/login.php
@@ -1,9 +1,18 @@
-<section>
-    <form action="<?= BASE_URL ?>/src/controllers/auth_controller.php?action=login" method="post">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Email" required>
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password" placeholder="Mot de passe" required>
-        <input type="submit" value="Login">
-    </form>
-</section>
+<header class="login-header">
+    <img class="login-header__logo" src="<?= getImagePath() ?>/logo.png" alt="Logo du restaurant">
+</header>
+
+<form action="<?= BASE_URL ?>/src/controllers/auth_controller.php?action=login" method="post" class="login-form">
+
+    <label for="email" class="login-form__label">Email
+        <input class="login-form__input" type="email" name="email" id="email" placeholder="Email" required>
+    </label>
+
+    <label for="password" class="login-form__label">Mot de passe
+        <input class="login-form__input" type="password" name="password" id="password" placeholder="Mot de passe" required>
+    </label>
+    
+    <p class="login-form__error">Email ou mot de passe incorrect </p>
+
+    <input type="submit" value="Login" class="button">
+</form>
