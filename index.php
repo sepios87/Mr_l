0e39b641fd423cb0d5d6c9a75f2a7fb49f430cfb
florian <florian.toribio@gmail.com>
2023-06-15 15:04:43 +0200

    script

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ require_once 'src/core/config.php';
 require_once 'src/core/utils.php';
 require_once 'src/core/autoload.php';
 
-// require_once 'src/shared/header.php';
-// require_once 'src/pages/home/home.php';
 require_once 'src/core/router.php';
 
 ?>
---
+++ mrl_script.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 15 juin 2023 à 13:03
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mrl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+CREATE TABLE IF NOT EXISTS `food` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `isVegetarian` tinyint(1) NOT NULL,
+  `ingredients` varchar(555) COLLATE utf8mb4_general_ci NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `day` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `hourly` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `schedule`
+--
+
+INSERT INTO `schedule` (`id`, `day`, `hourly`) VALUES
+(1, 'Lundi', '11H30 - 14H / 19H - 21H30'),
+(2, 'Mardi', '11H30 - 14H / 19H - 21H30'),
+(3, 'Mercredi', '11H30 - 14H / 19H - 21H30'),
+(4, 'Jeudi', '11H30 - 14H / 19H - 21H30'),
+(5, 'Vendredi', '11H30 - 14H / 19H - 21H30'),
+(6, 'Samedi', '11H30 - 14H / 19H - 21H30'),
+(7, 'Dimanche', 'Fermé');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(1, 'florian.toribio@gmail.com', 'c8f0e7d6a7a962341423ce4c2c05625dcf0d3938dffc71c81bf9ffe27d7384f7');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
