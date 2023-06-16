bb7be9c607f7d584c5e9c40148944c3fa53d93a9
florian <florian.toribio@gmail.com>
2023-06-16 11:33:49 +0200

    food

--- src/pages/manage-food/manage-food.php
+++ src/pages/manage-food/manage-food.php
@@ -6,32 +6,29 @@
 <section>
     <p class="manage-food__title">Modifiez le menu : </p>
     <section class="manage-food__container">
-        <ul class="manager-food-list">
-            <li class="manager-food-list__item">
-                <div>
-                    <p>Hot dog</p>
-                    <p>Sauce cool</p>
-                    <p>20€</p>
-                </div>
-                <button>Modifier</button>
-            </li>
-            <li class="manager-food-list__item">
-                <div>
-                    <p>Hot dog</p>
-                    <p>Sauce cool</p>
-                    <p>20€</p>
-                </div>
-                <button>Modifier</button>
-            </li>
-            <li class="manager-food-list__item">
-                <form class="manager-food-list__edit-form">
-                    <input type="text" placeholder="Nom du plat">
-                    <textarea type="text" placeholder="Ingrédients" ari></textarea>
-                    <input type="text" placeholder="Prix">
-                    <input type="submit" value="Valider">
-                </form>
+        <ul class="manage-food-list" id="manage-food-list">
+            <!-- TODO set id -->
+            <li class="manage-food-list__item" id="manage-food-item-1">
+                <p id="manage-food-item-name">Hot dog</p>
+                <p id="manage-food-item-ingredients">Pain, saucisse, sauce</p>
+                <p id="manage-food-item-price">20€</p>
+                <!-- TODO set id -->
+                <button class="button button--small button--grey" id="manage-food-edit-1" onclick="editFood(this)">Modifier</button>
             </li>
         </ul>
     </section>
-    <button class="button manage-food__add-button">Ajouter un élément</button>
+    <button id="manage-food-btn-add" class="button manage-food__add-button">Ajouter un élément</button>
 </section>
+
+<template id="manage-food-template">
+    <form class="manage-food-list__edit-form" action="<?= BASE_URL ?>/src/controllers/food_controller.php?action=update" method="POST">
+        <input type="text" placeholder="Nom du plat" id="name" class="manage-food-list__input">
+        <input type="text" placeholder="Ingrédients" id="ingredients" class="manage-food-list__input">
+        <input type="text" placeholder="Prix" id="price" class="manage-food-list__input">
+        <div class="manage-food-list__checkbox">
+            <label>Végétarien</label>
+            <input type="checkbox" name="vegetarian" value="vegetarien">
+        </div>
+        <input class="button button--small" type="submit" value="Valider">
+    </form>
+</template>
--- src/pages/manage-food/manage-food.script.js
+++ src/pages/manage-food/manage-food.script.js
@@ -1,1 +1,40 @@
-console.log('coucou');
+document.addEventListener('DOMContentLoaded', function () {
+    const foodList = document.getElementById('manage-food-list');
+    const foodTemplate = document.getElementById('manage-food-template');
+    const addBtn = document.getElementById('manage-food-btn-add');
+
+    addBtn.addEventListener('click', function () {
+        const li = document.createElement('li');
+        li.classList.add('manage-food-list__item');
+        li.appendChild(foodTemplate.content.cloneNode(true));
+        foodList.appendChild(li);
+    });
+});
+
+function editFood(elem) {
+    const slip = elem.id.split('-');
+    const id = slip[slip.length - 1];
+
+    const foodTemplate = document.getElementById('manage-food-template');
+    const nameInput = foodTemplate.content.querySelector('#name');
+    const priceInput = foodTemplate.content.querySelector('#price');
+    const ingredientsInput = foodTemplate.content.querySelector('#ingredients');
+
+    const item = document.getElementById(`manage-food-item-${id}`);
+    nameInput.value = item.querySelector('#manage-food-item-name').innerHTML;
+    priceInput.value = item.querySelector('#manage-food-item-price').innerHTML;
+    ingredientsInput.value = item.querySelector(
+        '#manage-food-item-ingredients'
+    ).innerHTML;
+
+    const inputId = document.createElement('input');
+    inputId.id = 'id';
+    inputId.setAttribute('type', 'hidden');
+    inputId.setAttribute('name', 'id');
+    inputId.setAttribute('value', id);
+    const form = foodTemplate.content.querySelector('.manage-food-list__edit-form');
+    form.appendChild(inputId);
+
+    item.innerHTML = '';
+    item.appendChild(foodTemplate.content.cloneNode(true));
+}
